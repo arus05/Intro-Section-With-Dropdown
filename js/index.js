8aae4760351b5ac53e323a3cc1cd7acaf8fdb5c0
Arus <arus.zh0905@gmail.com>
2023-03-17 13:17:54 +0800

    finished nav bar

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
 const heroBgImg = document.getElementById("hero-bg-img");
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("nav");
+const featuresNav = document.getElementById("features-link");
+const featuresList = document.getElementById("features-list");
+const companyNav = document.getElementById("company-link");
+const companyList = document.getElementById("company-list");
 
-function renderHeroBgImg(){
+function handleQuery800(){
   if(window.matchMedia("(min-width: 800px)").matches){
     heroBgImg.src = "./../images/image-hero-desktop.png";
   }
   else{
     heroBgImg.src = "./../images/image-hero-mobile.png";
   }
+  
+  nav.classList.remove("toggled");
 }
 
-window.matchMedia("(min-width: 800px)").addListener(renderHeroBgImg);
+window.matchMedia("(min-width: 800px)").addListener(handleQuery800);
 
+menuBtn.addEventListener("click", ()=>{
+  nav.classList.toggle("toggled");
+})
+
+featuresNav.addEventListener("click", (event)=>{
+  event.stopPropagation();
+  featuresList.classList.toggle("features-toggled");
+  if(featuresList.classList.contains("features-toggled")){
+    companyList.classList.remove("company-toggled");
+  }
+})
+
+companyNav.addEventListener("click", ()=>{
+  event.stopPropagation();
+  companyList.classList.toggle("company-toggled");
+  if(companyList.classList.contains("company-toggled")){
+    featuresList.classList.remove("features-toggled");
+  }
+})
+
+// document.body.addEventListener("click", ()=>{
+//   featuresList.classList.remove("features-toggled");
+//   companyList.classList.remove("company-toggled");
+// })
